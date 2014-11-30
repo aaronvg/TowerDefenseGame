1b2766a03c4216090acc8d9274d507ffcf242ee4
naughtyfiddle <ctdinh93@gmail.com>
2014-11-30 02:41:58 -0600

    Tower menu now rotates to face camera
    also moved tower scripts to their own folder because wow

---
+++ Assets/Scripts/Tower/TowerMenuController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerMenuController : MonoBehaviour {
+
+	private Camera camera;
+
+	// Use this for initialization
+	void Start () {
+		camera = GameObject.FindObjectOfType<Camera>();
+	}
+	
+	void Update () {
+		Vector3 cameraRotation = camera.transform.rotation.eulerAngles;
+		cameraRotation.x = 24;
+		cameraRotation.z = 0;
+		transform.eulerAngles = cameraRotation;
+	}
+}
